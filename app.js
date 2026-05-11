ce762c1457fbd869fffd03aff5881b8361d0b1c0
Dumindudasun <dumindudd20@gmail.com>
2026-05-11 22:30:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("GitHub Actions CI/CD Demo");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+
+
